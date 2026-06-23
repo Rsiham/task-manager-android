92afac889ae91bc6a018fa1b501110eef75f0085
Rsiham <128421471+Rsiham@users.noreply.github.com>
2026-06-23 15:23:15 -0400

    Update MainActivity.kt

--- MainActivity.kt
+++ MainActivity.kt
@@ -1,10 +1,17 @@
-package com.example.taskmanager
+package com.example.weatherapp
 
 class MainActivity {
 
-    private val repository = TaskRepository()
+    private val repository = WeatherRepository()
 
-    fun loadTasks() {
-        println(repository.getTasks())
+    fun afficherMeteo() {
+
+        val weather = repository.getWeather("Montréal")
+
+        println("Ville : ${weather.city}")
+        println("Température : ${weather.temperature}°C")
+        println("Humidité : ${weather.humidity}%")
+        println("Vent : ${weather.windSpeed} km/h")
+        println("Description : ${weather.description}")
     }
 }
